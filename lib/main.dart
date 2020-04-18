08bc315357bbf9800aa6b21c8309111fcb19c9a6
Randula Wickramasuriya <randularsw@gmail.com>
2020-04-18 21:31:09 +0530

    ui design

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('1 Clicked');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('6 Clicked');
+              },
+              child: Image.asset('images/dice6.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
